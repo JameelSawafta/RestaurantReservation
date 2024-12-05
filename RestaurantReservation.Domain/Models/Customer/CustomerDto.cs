daf379ad04467beb85ae42cb35335ffb11a4b805
JameelSawafta <jameel.wlar999@gmail.com>
2024-12-06 00:47:01 +0200

    Add Customer Models

---
+++ RestaurantReservation.Domain/Models/Customer/CustomerDto.cs
@@ -0,0 +1,10 @@
+namespace RestaurantReservation.Domain.Models.Customer;
+
+public class CustomerDto
+{
+    public Guid CustomerId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
---
+++ RestaurantReservation.Domain/Models/Customer/UpdateCustomerDto.cs
@@ -0,0 +1,9 @@
+namespace RestaurantReservation.Domain.Models.Customer;
+
+public class UpdateCustomerDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
