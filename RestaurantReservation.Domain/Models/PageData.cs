0d70b53894569d64866eab7e7bfb7d23e930466c
JameelSawafta <jameel.wlar999@gmail.com>
2024-12-27 22:40:53 +0200

    update pageData

--- RestaurantReservation.Domain/Models/PageData.cs
+++ RestaurantReservation.Domain/Models/PageData.cs
@@ -6,17 +6,12 @@ public class PageData
     public int TotalPageCount { get; set; }
     public int PageSize { get; set; }
     public int CurrentPage { get; set; }
-    public string PreviousPageLink { get; set; }
-    public string NextPageLink { get; set; }
 
-    public PageData(int totalItemCount, int pageSize, int currentPage, Func<int, string> generatePageLink)
+    public PageData(int totalItemCount, int pageSize, int currentPage)
     {
         TotalItemCount = totalItemCount;
         PageSize = pageSize;
         CurrentPage = currentPage;
         TotalPageCount = (int)Math.Ceiling(totalItemCount / (double)pageSize);
-        
-        PreviousPageLink = currentPage > 1 ? generatePageLink(currentPage - 1) : null;
-        NextPageLink = currentPage < TotalPageCount ? generatePageLink(currentPage + 1) : null;
     }
 }
